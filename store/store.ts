adb1e4290cc73f2855f6abccda9d7292a0fad924
mashashelegeda <masha0shelegeda@gmail.com>
2026-01-13 14:58:07 +0100

    Store change

--- store/store.ts
+++ store/store.ts
@@ -6,12 +6,15 @@ import settingsReducer from './slices/settingSlice';
 const rootReducer = combineReducers({
     CurrentWeatherSliceReducer,
     ForecastSliceReducer,
-    settings: settingsReducer, // 👈 ИМЯ ВАЖНО
+    settings: settingsReducer,
 });
 
 export const store = configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
+export type AppDispatch = AppStore['dispatch'];
